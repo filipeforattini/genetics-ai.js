9ca6a543aa9fb1c0bfc9d01922002e47d9cdebfc
Filipe Forattini <filipe@vitta.me>
2023-04-04 08:52:14 -0300

    chore: multi build file with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -7,30 +7,56 @@ import builtins from 'rollup-plugin-node-builtins';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'src/index.js',
+export default [
+  {
+    input: 'src/index.js',
 
-  output: {
-    format: 'cjs',
-    file: './dist/app.js'
+    output: {
+      format: 'cjs',
+      file: './dist/app.min.js'
+    },
+
+    plugins: [
+      globals(),
+      builtins(),
+      nodePolyfills(),
+      commonjs(),
+      nodeResolve({
+        main: true,
+        jsnext: true,
+        browser: true,
+      }),
+      filesize(),
+      minify({
+        mangle: true
+      }),
+      uglify({
+        mangle: true
+      }),
+    ],
   },
+  {
+    input: 'src/index.js',
 
-  plugins: [
-    globals(),
-    builtins(),
-    nodePolyfills(),
-    commonjs(),
-    nodeResolve({
-      main: true,
-      jsnext: true,
-      browser: true,
-    }),
-    filesize(),
-    minify({
-      mangle: true
-    }),
-    uglify({
-      mangle: true
-    }),
-  ],
-};
+    output: {
+      format: 'cjs',
+      file: './dist/app.js'
+    },
+
+    plugins: [
+      globals(),
+      builtins(),
+      nodePolyfills(),
+      commonjs(),
+      nodeResolve({
+        main: true,
+        jsnext: true,
+        browser: true,
+      }),
+      filesize(),
+      minify({
+        mangle: true
+      }),
+    ],
+  },
+];
